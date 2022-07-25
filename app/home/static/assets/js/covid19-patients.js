ed021610607efaa356c41e8bb2be0c1d81b87ba8
carloscastillo10 <carloscastillo090916@gmail.com>
2022-07-25 11:43:12 -0500

    :sparkles: Adding patients line chart

--- app/home/static/assets/js/covid19-patients.js
+++ app/home/static/assets/js/covid19-patients.js
@@ -1,5 +1,5 @@
-moderatePatientColor = '#17c1e8';
-seriusPatientColor = '#3A416F';
+moderatePatientColor = '#10739E';
+seriusPatientColor = '#76D7C4';
 criticalPatientColor = '#cb0c9f';
 
 patientsStatus = [
@@ -177,7 +177,125 @@ new Chart(ctx2, {
 if (document.querySelector('.datepicker')) {
     flatpickr('.datepicker', {
         mode: 'range',
-        locale: 'es'
+        locale: 'es',
         // defaultDate: "2022-01-01 to 2022-07-24"
     });
 }
+
+var ctx3 = document.getElementById('chart-patients-line').getContext('2d');
+var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+gradientStroke1.addColorStop(1, 'rgba(23, 194, 232, 0.2)');
+gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+gradientStroke1.addColorStop(0, 'rgba(23, 194, 232,0)');
+
+var gradientStroke2 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+gradientStroke2.addColorStop(1, 'rgba(58, 65, 111,0.2)');
+gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+gradientStroke2.addColorStop(0, 'rgba(58, 65, 111,0)');
+
+var gradientStroke3 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+gradientStroke3.addColorStop(1, 'rgba(203, 12, 159,0.2)');
+gradientStroke3.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+gradientStroke3.addColorStop(0, 'rgba(203, 12, 159,0)');
+
+new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: [
+            'Enero',
+            'Febrero',
+            'Marzo',
+            'Abril',
+            'Mayo',
+            'Junio',
+            'Julio',
+        ],
+        datasets: [
+            {
+                label: 'Pacientes moderados',
+                tension: 0.4,
+                borderWidth: 0,
+                pointRadius: 2,
+                pointBackgroundColor: moderatePatientColor,
+                borderColor: moderatePatientColor,
+                borderWidth: 3,
+                backgroundColor: gradientStroke1,
+                fill: true,
+                data: [50, 40, 300, 220, 500, 250, 400],
+                maxBarThickness: 6,
+            },
+            {
+                label: 'Pacientes graves',
+                tension: 0.4,
+                borderWidth: 0,
+                pointRadius: 2,
+                pointBackgroundColor: seriusPatientColor,
+                borderColor: seriusPatientColor,
+                borderWidth: 3,
+                backgroundColor: gradientStroke2,
+                fill: true,
+                data: [30, 90, 40, 140, 290, 290, 340],
+                maxBarThickness: 6,
+            },
+            {
+                label: 'Pacientes críticos',
+                tension: 0.4,
+                borderWidth: 0,
+                pointRadius: 2,
+                pointBackgroundColor: criticalPatientColor,
+                borderColor: criticalPatientColor,
+                borderWidth: 3,
+                backgroundColor: gradientStroke3,
+                fill: true,
+                data: [40, 80, 70, 90, 30, 90, 140],
+                maxBarThickness: 6,
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        interaction: {
+            intersect: false,
+            mode: 'index',
+        },
+        scales: {
+            y: {
+                grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                },
+                ticks: {
+                    display: false,
+                },
+            },
+            x: {
+                grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                },
+                ticks: {
+                    beginAtZero: true,
+                    font: {
+                        size: 12,
+                        family: 'Open Sans',
+                        style: 'normal',
+                    },
+                    color: '#9ca2b7',
+                },
+            },
+        },
+    },
+});
