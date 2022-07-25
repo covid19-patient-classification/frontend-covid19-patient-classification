d7b18fc48d4c529872a31febc3de2e942219c9a6
carloscastillo10 <carloscastillo090916@gmail.com>
2022-07-24 20:13:23 -0500

    :art: Adding initial styles of patients by week chart

--- app/home/static/assets/js/covid19-patients.js
+++ app/home/static/assets/js/covid19-patients.js
@@ -1,6 +1,6 @@
-moderatePatientColor = '#17c1e8'
-seriusPatientColor = '#3A416F'
-criticalPatientColor = '#cb0c9f'
+moderatePatientColor = '#17c1e8';
+seriusPatientColor = '#3A416F';
+criticalPatientColor = '#cb0c9f';
 
 patientsStatus = [
     'moderate-status',
@@ -45,7 +45,9 @@ new Chart(ctx1, {
                 pointRadius: 2,
                 borderWidth: 2,
                 backgroundColor: [
-                    moderatePatientColor, seriusPatientColor, criticalPatientColor
+                    moderatePatientColor,
+                    seriusPatientColor,
+                    criticalPatientColor,
                 ],
                 data: [140, 60, 18],
                 fill: false,
@@ -86,6 +88,89 @@ new Chart(ctx1, {
                 ticks: {
                     display: false,
                 },
+            },
+        },
+    },
+});
+
+// Chart bar Patients by week
+var ctx = document.getElementById('chart-week-patients').getContext('2d');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['18', '19', '20', '21', '22', '23', '24'],
+        datasets: [
+            {
+                label: 'Pacientes moderados',
+                tension: 1,
+                borderWidth: 0,
+                borderRadius: 0,
+                borderSkipped: false,
+                backgroundColor: moderatePatientColor,
+                data: [10, 10, 5, 7, 5, 5, 5],
+                maxBarThickness: 10,
+            },
+            {
+                label: 'Pacientes graves',
+                tension: 2,
+                borderWidth: 0,
+                borderRadius: 4,
+                borderSkipped: false,
+                backgroundColor: seriusPatientColor,
+                data: [3, 3, 3, 5, 3, 3, 3],
+                maxBarThickness: 10,
+            },
+            {
+                label: 'Pacientes críticos',
+                tension: 2,
+                borderWidth: 0,
+                borderRadius: 0,
+                borderSkipped: false,
+                backgroundColor: criticalPatientColor,
+                data: [2, 3, 3, 3, 3, 3, 3],
+                maxBarThickness: 10,
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        scales: {
+            y: {
+                grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                },
+                ticks: {
+                    display: false,
+                },
+                stacked: true
+            },
+            x: {
+                grid: {
+                    drawBorder: false,
+                    display: false,
+                    drawOnChartArea: false,
+                    drawTicks: false,
+                },
+                ticks: {
+                    beginAtZero: true,
+                    font: {
+                        size: 12,
+                        family: 'Open Sans',
+                        style: 'normal',
+                    },
+                    color: '#9ca2b7',
+                },
+                stacked: true
             },
         },
     },
