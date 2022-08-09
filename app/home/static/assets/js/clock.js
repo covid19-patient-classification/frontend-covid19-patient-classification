3b4527da6cb162015559f69c7164c2e565d90cb0
carloscastillo10 <carloscastillo090916@gmail.com>
2022-08-09 16:55:01 -0500

    :fire: Remove seconds of clock

--- app/home/static/assets/js/clock.js
+++ app/home/static/assets/js/clock.js
@@ -1,52 +1,51 @@
 function startTime() {
-  var today = new Date();
-  var hr = today.getHours();
-  var min = today.getMinutes();
-  var sec = today.getSeconds();
-  //Add a zero in front of numbers<10
-  hr = checkTime(hr);
-  min = checkTime(min);
-  sec = checkTime(sec);
-  document.getElementById("clock").innerHTML =
-    hr + ":" + min + ":" + sec;
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
 
-  var months = [
-    "Enero",
-    "Febrero",
-    "Marazo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre",
-  ];
-  var days = [
-    "Domingo",
-    "Lunes",
-    "Martes",
-    "Miércoles",
-    "Jueves",
-    "Viernes",
-    "Sábado",
-  ];
-  var curWeekDay = days[today.getDay()];
-  var curDay = today.getDate();
-  var curMonth = months[today.getMonth()];
-  var curYear = today.getFullYear();
-  var date = curWeekDay + ", " + curDay + " " + curMonth + " " + curYear;
-  document.getElementById("date").innerHTML = date;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    document.getElementById('clock').innerHTML = hr + ':' + min;
 
-  var time = setTimeout(function () {
-    startTime();
-  }, 500);
+    var months = [
+        'Enero',
+        'Febrero',
+        'Marazo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre',
+    ];
+    var days = [
+        'Domingo',
+        'Lunes',
+        'Martes',
+        'Miércoles',
+        'Jueves',
+        'Viernes',
+        'Sábado',
+    ];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay + ', ' + curDay + ' ' + curMonth + ' ' + curYear;
+    document.getElementById('date').innerHTML = date;
+
+    var time = setTimeout(function () {
+        startTime();
+    }, 500);
 }
+
 function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
 }
