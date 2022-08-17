295fb6eec770fb5ca33d99ac4f426be0543642dd
carloscastillo10 <carloscastillo090916@gmail.com>
2022-08-17 15:26:53 -0500

    :sparkles: Redirect dashboard endpoint to root endpoint

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,8 +1,13 @@
 from app.dashboard import blueprint
-from flask import render_template
+from flask import render_template, redirect
 
 
 @blueprint.route('/')
 @blueprint.route('/index')
 def index():
     return render_template('home/dashboard.html', segment='dashboard')
+
+
+@blueprint.route('/dashboard')
+def dashboard():
+    return redirect('/')
