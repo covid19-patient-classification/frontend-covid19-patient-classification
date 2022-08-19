8604bbd222289caaa69e464beda4a7b8fa868951
carloscastillo10 <carloscastillo090916@gmail.com>
2022-08-19 18:31:57 -0500

    :hammer: Adding clean structure in light-mode-handler

--- app/home/static/assets/js/light-mode-handler.js
+++ app/home/static/assets/js/light-mode-handler.js
@@ -1,126 +1,148 @@
-"use strict";
+'use strict';
 
 // Verify navbar blur on scroll
 navbarBlurOnScroll('navbarBlur');
 
 // initialization of Tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
 
 // when input is focused add focused class for style
 function focused(el) {
-  if (el.parentElement.classList.contains('input-group')) {
-    el.parentElement.classList.add('focused');
-  }
+    if (el.parentElement.classList.contains('input-group')) {
+        el.parentElement.classList.add('focused');
+    }
 }
 
 // when input is focused remove focused class for style
 function defocused(el) {
-  if (el.parentElement.classList.contains('input-group')) {
-    el.parentElement.classList.remove('focused');
-  }
+    if (el.parentElement.classList.contains('input-group')) {
+        el.parentElement.classList.remove('focused');
+    }
 }
 
 // helper for adding on all elements multiple attributes
 function setAttributes(el, options) {
-  Object.keys(options).forEach(function(attr) {
-    el.setAttribute(attr, options[attr]);
-  })
+    Object.keys(options).forEach(function (attr) {
+        el.setAttribute(attr, options[attr]);
+    });
 }
 
 // adding on inputs attributes for calling the focused and defocused functions
 if (document.querySelectorAll('.input-group').length != 0) {
-  var allInputs = document.querySelectorAll('input.form-control');
-  allInputs.forEach(el => setAttributes(el, {
-    "onfocus": "focused(this)",
-    "onfocusout": "defocused(this)"
-  }));
+    var allInputs = document.querySelectorAll('input.form-control');
+    allInputs.forEach((el) =>
+        setAttributes(el, {
+            onfocus: 'focused(this)',
+            onfocusout: 'defocused(this)',
+        })
+    );
 }
-
 
 // Set Navbar Fixed
 function navbarFixed(el) {
-  let classes = ['position-sticky', 'blur', 'shadow-blur', 'mt-4', 'left-auto', 'top-2', 'z-index-sticky'];
-  const navbar = document.getElementById('navbarBlur');
+    let classes = [
+        'position-sticky',
+        'blur',
+        'shadow-blur',
+        'mt-4',
+        'left-auto',
+        'top-2',
+        'z-index-sticky',
+    ];
+    const navbar = document.getElementById('navbarBlur');
 
-  if (!el.getAttribute("checked")) {
-    navbar.classList.add(...classes);
-    navbar.setAttribute('navbar-scroll', 'true');
-    navbarBlurOnScroll('navbarBlur');
-    el.setAttribute("checked", "true");
-  } else {
-    navbar.classList.remove(...classes);
-    navbar.setAttribute('navbar-scroll', 'false');
-    navbarBlurOnScroll('navbarBlur');
-    el.removeAttribute("checked");
-  }
-};
-
-// Navbar blur on scroll
-
-function navbarBlurOnScroll(id) {
-  const navbar = document.getElementById(id);
-  let navbarScrollActive = navbar ? navbar.getAttribute("navbar-scroll") : false;
-  let scrollDistance = 5;
-  let classes = ['position-sticky', 'blur', 'shadow-blur', 'mt-4', 'left-auto', 'top-2', 'z-index-sticky'];
-  let toggleClasses = ['shadow-none'];
-
-  if (navbarScrollActive == 'true') {
-    window.onscroll = debounce(function() {
-      if (window.scrollY > scrollDistance) {
-        blurNavbar();
-      } else {
-        transparentNavbar();
-      }
-    }, 10);
-  } else {
-    window.onscroll = debounce(function() {
-      transparentNavbar();
-    }, 10);
-  }
-
-  function blurNavbar() {
-    navbar.classList.add(...classes)
-    navbar.classList.remove(...toggleClasses)
-
-    toggleNavLinksColor('blur');
-  }
-
-  function transparentNavbar() {
-    if (navbar) {
-      navbar.classList.remove(...classes)
-      navbar.classList.add(...toggleClasses)
-
-      toggleNavLinksColor('transparent');
+    if (!el.getAttribute('checked')) {
+        navbar.classList.add(...classes);
+        navbar.setAttribute('navbar-scroll', 'true');
+        navbarBlurOnScroll('navbarBlur');
+        el.setAttribute('checked', 'true');
+    } else {
+        navbar.classList.remove(...classes);
+        navbar.setAttribute('navbar-scroll', 'false');
+        navbarBlurOnScroll('navbarBlur');
+        el.removeAttribute('checked');
     }
-  }
-
-  function toggleNavLinksColor(type) {
-    let navLinks = document.querySelectorAll('.navbar-main .nav-link')
-    let navLinksToggler = document.querySelectorAll('.navbar-main .sidenav-toggler-line')
-
-    if (type === "blur") {
-      navLinks.forEach(element => {
-        element.classList.remove('text-body')
-      });
-
-      navLinksToggler.forEach(element => {
-        element.classList.add('bg-dark')
-      });
-    } else if (type === "transparent") {
-      navLinks.forEach(element => {
-        element.classList.add('text-body')
-      });
-
-      navLinksToggler.forEach(element => {
-        element.classList.remove('bg-dark')
-      });
-    }
-  }
 }
 
+// Navbar blur on scroll
+function navbarBlurOnScroll(id) {
+    const navbar = document.getElementById(id);
+    let navbarScrollActive = navbar
+        ? navbar.getAttribute('navbar-scroll')
+        : false;
+    let scrollDistance = 5;
+    let classes = [
+        'position-sticky',
+        'blur',
+        'shadow-blur',
+        'mt-4',
+        'left-auto',
+        'top-2',
+        'z-index-sticky',
+    ];
+    let toggleClasses = ['shadow-none'];
+
+    if (navbarScrollActive == 'true') {
+        window.onscroll = debounce(function () {
+            if (window.scrollY > scrollDistance) {
+                blurNavbar();
+            } else {
+                transparentNavbar();
+            }
+        }, 10);
+    } else {
+        window.onscroll = debounce(function () {
+            transparentNavbar();
+        }, 10);
+    }
+
+    function blurNavbar() {
+        navbar.classList.add(...classes);
+        navbar.classList.remove(...toggleClasses);
+
+        toggleNavLinksColor('blur');
+    }
+
+    function transparentNavbar() {
+        if (navbar) {
+            navbar.classList.remove(...classes);
+            navbar.classList.add(...toggleClasses);
+
+            toggleNavLinksColor('transparent');
+        }
+    }
+
+    function toggleNavLinksColor(type) {
+        let navLinks = document.querySelectorAll('.navbar-main .nav-link');
+        let navLinksToggler = document.querySelectorAll(
+            '.navbar-main .sidenav-toggler-line'
+        );
+
+        if (type === 'blur') {
+            navLinks.forEach((element) => {
+                element.classList.remove('text-body');
+            });
+
+            navLinksToggler.forEach((element) => {
+                element.classList.add('bg-dark');
+            });
+        } else if (type === 'transparent') {
+            navLinks.forEach((element) => {
+                element.classList.add('text-body');
+            });
+
+            navLinksToggler.forEach((element) => {
+                element.classList.remove('bg-dark');
+            });
+        }
+    }
+}
 
 // Debounce Function
 // Returns a function, that, as long as it continues to be invoked, will not
@@ -128,20 +150,20 @@ function navbarBlurOnScroll(id) {
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
 function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this,
-      args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+}
 
 // Toggle Sidenav
 const iconNavbarSidenav = document.getElementById('iconNavbarSidenav');
@@ -151,41 +173,40 @@ let body = document.getElementsByTagName('body')[0];
 let className = 'g-sidenav-pinned';
 
 if (iconNavbarSidenav) {
-  iconNavbarSidenav.addEventListener("click", toggleSidenav);
+    iconNavbarSidenav.addEventListener('click', toggleSidenav);
 }
 
 if (iconSidenav) {
-  iconSidenav.addEventListener("click", toggleSidenav);
+    iconSidenav.addEventListener('click', toggleSidenav);
 }
 
 function toggleSidenav() {
-  if (body.classList.contains(className)) {
-    body.classList.remove(className);
-  } else {
-    body.classList.add(className);
+    if (body.classList.contains(className)) {
+        body.classList.remove(className);
+    } else {
+        body.classList.add(className);
 
-    iconSidenav.classList.remove('d-none');
-  }
+        iconSidenav.classList.remove('d-none');
+    }
 }
 
 // Resize navbar color depends on configurator active type of sidenav
 
 let referenceButtons = document.querySelector('[data-class]');
 
-
 // Deactivate sidenav type buttons on resize and small screens
-window.addEventListener("resize", sidenavTypeOnResize);
-window.addEventListener("load", sidenavTypeOnResize);
+window.addEventListener('resize', sidenavTypeOnResize);
+window.addEventListener('load', sidenavTypeOnResize);
 
 function sidenavTypeOnResize() {
-  let elements = document.querySelectorAll('[onclick="sidebarType(this)"]');
-  if (window.innerWidth < 1200) {
-    elements.forEach(function(el) {
-      el.classList.add('disabled');
-    });
-  } else {
-    elements.forEach(function(el) {
-      el.classList.remove('disabled');
-    });
-  }
+    let elements = document.querySelectorAll('[onclick="sidebarType(this)"]');
+    if (window.innerWidth < 1200) {
+        elements.forEach(function (el) {
+            el.classList.add('disabled');
+        });
+    } else {
+        elements.forEach(function (el) {
+            el.classList.remove('disabled');
+        });
+    }
 }
