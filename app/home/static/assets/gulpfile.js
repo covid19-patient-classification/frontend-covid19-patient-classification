9a99f4d8ed6b49bc055f606397aa2ff4de5982ab
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-12 19:14:47 -0500

    :racehorse: Minify core js files

--- app/home/static/assets/gulpfile.js
+++ app/home/static/assets/gulpfile.js
@@ -1,24 +1,41 @@
 var gulp = require('gulp');
 var path = require('path');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 
 var Paths = {
     HERE: './',
     DIST: 'dist/',
     CSS: './css/',
+    JS: './js/',
 };
 
 // Minify CSS
-gulp.task('minify:css', function() {
-    return gulp.src(Paths.CSS + '/*.css')
+gulp.task('minify:css', function () {
+    return gulp
+        .src(Paths.CSS + '/*.css')
         .pipe(cleanCss())
-        .pipe(rename(function(path) {
-            // Updates the object in-place
-            path.extname = ".min.css";
-        }))
-        .pipe(gulp.dest(Paths.CSS))
+        .pipe(
+            rename(function (path) {
+                path.extname = '.min.css';
+            })
+        )
+        .pipe(gulp.dest(Paths.CSS));
+});
+
+// Minify JS
+gulp.task('minify:js', function () {
+    return gulp
+        .src(Paths.JS + '/*.js')
+        .pipe(uglify())
+        .pipe(
+            rename(function (path) {
+                path.extname = '.min.js';
+            })
+        )
+        .pipe(gulp.dest(Paths.JS + Paths.DIST));
 });
 
 // Default
-gulp.task('default', gulp.series('minify:css'));
+gulp.task('default', gulp.series('minify:css', 'minify:js'));
