40b140697c6bd207b740caf2bedf05ba00541a96
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-21 13:06:44 -0500

    :zap: Refactor classify patient endpoint

--- app/patient/routes.py
+++ app/patient/routes.py
@@ -1,12 +1,14 @@
 from app.patient import blueprint
-from flask import render_template, jsonify
+from flask import abort, render_template, jsonify, request
 
 
-@blueprint.route('/new-patient')
-def index():
-    return render_template('home/new-patient.html', segment='patient', page_name='Clasificar Paciente')
-
-
-@blueprint.route('/classify-patient', methods=['POST'])
+@blueprint.route('/classify-patient', methods=['GET', 'POST'])
 def classify_patient():
-    return jsonify({'type': 'critical'}, 200)
+    try:
+        if request.method == 'GET':
+            return render_template('home/classify-patient.html', segment='patient', page_name='Clasificar Paciente')
+
+        print(request.form.to_dict())
+        return jsonify({'type': 'critical'}, 200)
+    except Exception:
+        abort(500)
