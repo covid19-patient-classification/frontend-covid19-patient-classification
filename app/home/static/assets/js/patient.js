863ec5cd5f83635ef01e5b79e738186348a346ea
carloscastillo10 <carloscastillo090916@gmail.com>
2022-10-10 14:23:04 -0500

    :bug: Fix clinic variables title tooltip

--- app/home/static/assets/js/patient.js
+++ app/home/static/assets/js/patient.js
@@ -8,40 +8,19 @@ const pfRatioInput = document.getElementById('pf-ratio');
 const pfRatioTooltip = document.getElementById('pf-ratio-tooltip');
 const ardsTooltip = document.getElementById('ards-tooltip');
 
-initializeClinicalTooltips();
-
-pao2Input.addEventListener('keyup', () => {
-    calculatePfRatio();
-});
-
-fio2Input.addEventListener('keyup', () => {
-    calculatePfRatio();
-});
-
-patientForm.addEventListener('submit', (event) => {
-    if (patientForm.checkValidity()) {
-        var patientFormData = formDataToJson(event);
-        sendPatient(patientFormData);
-    } else {
-        emptyFormAlert();
-    }
-    event.preventDefault();
-    
-});
-
-function initializeClinicalTooltips(){
-    setBoostrapTooltip(sato2Tooltip, '95 - 100%', '90 - 94%', '<90%') // SatO2 tooltip
-    setBoostrapTooltip(paa2Tooltip, '61 - 100%', '50 - 60%', '<50%') // PaO2 tooltip
-    setBoostrapTooltip(fio2Tooltip, '1 - 20%', '21 - 40%', '>40%') // FiO2 tooltip
-    setGeneralTooltip(pfRatioTooltip) // P/F Ratio tootltip
-    setGeneralTooltip(ardsTooltip) // ARDS tooltip
+function initializeClinicalTooltips() {
+    setBoostrapTooltip(sato2Tooltip, 'Saturación de oxígeno', '95 - 100%', '90 - 94%', '<90%'); // SatO2 tooltip
+    setBoostrapTooltip(paa2Tooltip, 'Presión parcial de oxígeno', '61 - 100%', '50 - 60%', '<50%'); // PaO2 tooltip
+    setBoostrapTooltip(fio2Tooltip, 'Fracción de oxígeno inspirado', '1 - 20%', '21 - 40%', '>40%'); // FiO2 tooltip
+    setGeneralTooltip(pfRatioTooltip); // P/F Ratio tootltip
+    setGeneralTooltip(ardsTooltip); // ARDS tooltip
 }
 
-function setBoostrapTooltip(tooltipId, normalValue, lowValue, criticalValue){
+function setBoostrapTooltip(tooltipId, tooltipTitle, normalValue, lowValue, criticalValue) {
     return new bootstrap.Tooltip(tooltipId, {
         html: true,
-        title: displayTooltipHTML(normalValue, lowValue, criticalValue),
-    })
+        title: displayTooltipHTML(tooltipTitle, normalValue, lowValue, criticalValue),
+    });
 }
 
 function calculatePfRatio() {
@@ -51,8 +30,8 @@ function calculatePfRatio() {
     if (pao2 >= 0 && pao2 <= 100 && fio2 >= 1 && fio2 <= 100) {
         var pfRatio = (pao2 / fio2) * 100;
         pfRatioInput.value = pfRatio.toFixed(1);
-    }else{
-        pfRatioInput.value = ""
+    } else {
+        pfRatioInput.value = '';
     }
 }
 
@@ -70,22 +49,23 @@ function sendPatient(patientFormData) {
         },
         error: () => {
             errorAlert();
-        }
+        },
     });
 }
 
-function formDataToJson(event){
+function formDataToJson(event) {
     const patientFormData = new FormData(event.target);
     const patientFormObject = Object.fromEntries(patientFormData.entries());
-    var notCheckedInputs = Array.from(document.querySelectorAll('input[type="checkbox"]:not(:checked)'));
-    if (notCheckedInputs.length > 0){
+    var notCheckedInputs = Array.from(
+        document.querySelectorAll('input[type="checkbox"]:not(:checked)')
+    );
+    if (notCheckedInputs.length > 0) {
         notCheckedInputs.map((notCheckedInput) => {
-            patientFormObject[notCheckedInput.name] = "False";
-        })
+            patientFormObject[notCheckedInput.name] = 'False';
+        });
     }
     return patientFormObject;
 }
-
 
 function loadingAlert() {
     Swal.fire({
@@ -107,7 +87,7 @@ function emptyFormAlert() {
         text: 'Debe agregar todas las variables clínicas',
         confirmButtonText: 'Aceptar',
         timer: 3000,
-        timerProgressBar: true
+        timerProgressBar: true,
     });
 }
 
@@ -118,51 +98,71 @@ function errorAlert() {
         text: 'Intente nuevamente por favor!',
         confirmButtonText: 'Aceptar',
         timer: 3000,
-        timerProgressBar: true
+        timerProgressBar: true,
     });
 }
 
 function classificationDetailsAlert(response) {
     Swal.fire({
+        icon: 'success',
         title: 'Clasificado exitosamente',
         html: displayTimeLineHTML(response),
-        icon: 'success',
         confirmButtonText: 'De acuerdo',
-    }).then( () => {
-        window.location.reload(); 
+    }).then(() => {
+        window.location.reload();
     });
 }
 
-function displayTooltipHTML(normalValue, lowValue, criticalValue){
-    return `
-        <b>Saturación de oxígeno</b>
+function unsavedChangesAlert() {
+    Swal.fire({
+        icon: 'warning',
+        title: '¿Deseas abandonar el sitio?',
+        text: 'Es posible que los cambios que implementaste no se puedan guardar',
+        confirmButtonText: 'Abandonar',
+    }).then(() => {
+        window.location.reload();
+    });
+}
+
+function displayTooltipHTML(tooltipTitle, normalValue, lowValue, criticalValue) {
+    return (
+        `
+        <b>${tooltipTitle}</b>
         <div class='d-flex mt-2'>
             <span class='badge badge-md badge-dot text-justify p-0'>
                 <i class=' bg-gradient-success ms-0'></i>
-                <span class='text-xxs'>`+normalValue+ ` Normal</span>
+                <span class='text-xxs'>` +
+        normalValue +
+        ` Normal</span>
             </span>
         </div>
         <div class='d-flex mt-2'>
             <span class='badge badge-md badge-dot text-justify p-0'>
                 <i class='bg-gradient-warning ms-0'></i>
-                <span class='text-xxs'>`+lowValue +` Bajo</span>
+                <span class='text-xxs'>` +
+        lowValue +
+        ` Bajo</span>
             </span>
         </div>
         <div class='d-flex mt-2'>
             <span class='badge badge-md badge-dot text-justify p-0'>
                 <i class='bg-gradient-danger ms-0'></i>
-                <span class='text-xxs'>`+criticalValue+ ` Crítico</span>
+                <span class='text-xxs'>` +
+        criticalValue +
+        ` Crítico</span>
             </span>
         </div>                                         
-    `;
+    `
+    );
 }
-
 
 function displayTimeLineHTML(response) {
     return `
         <div class="container-fluid py-4">
             <div class="row gx-4 text-sm-start">
-                <h6 class="text-start">Es un paciente ${setSeverityTimeLine(response.covid19_severity_prediction)}</h6>
+                <h6 class="text-start">Es un paciente ${setSeverityTimeLine(
+                    response.covid19_severity_prediction
+                )}</h6>
                 <div class="timeline timeline-one-side mt-3 ps-3" data-timeline-axis-style="dashed">
                     <div class="timeline-block mb-4">
                         <span class="timeline-step">
@@ -171,8 +171,12 @@ function displayTimeLineHTML(response) {
                         <div class="timeline-content">
                             <h6 class="text-dark text-sm font-weight-bold mb-0 text-start">¿Saturación de O<sub>2</sub> < al 94%?</h6>
                             <div class="d-flex align-items-center mt-2">
-                                ${setIconTimeLine(response.decision_rules.sato2 < 94)}
-                                <p class="text-sm text-secondary fw-bolder mt-1 mb-0">${response.decision_rules.sato2}%</p>
+                                ${setIconTimeLine(
+                                    response.decision_rules.sato2 < 94
+                                )}
+                                <p class="text-sm text-secondary fw-bolder mt-1 mb-0">${
+                                    response.decision_rules.sato2
+                                }%</p>
                             </div>
                         </div>
                     </div>
@@ -183,8 +187,12 @@ function displayTimeLineHTML(response) {
                         <div class="timeline-content">
                             <h6 class="text-dark text-sm font-weight-bold mb-0 text-start">¿Presión Arterial / Fracción de O<sub>2</sub> inspirado < a 300 mmHg?</h6>
                             <div class="d-flex align-items-center mt-2">
-                                ${setIconTimeLine(response.decision_rules.pf_ratio < 300)}
-                                <p class="text-sm text-secondary fw-bolder mt-1 mb-0">${response.decision_rules.pf_ratio} mmHg</p>
+                                ${setIconTimeLine(
+                                    response.decision_rules.pf_ratio < 300
+                                )}
+                                <p class="text-sm text-secondary fw-bolder mt-1 mb-0">${
+                                    response.decision_rules.pf_ratio
+                                } mmHg</p>
                             </div>
                         </div>
                     </div>
@@ -195,7 +203,9 @@ function displayTimeLineHTML(response) {
                         <div class="timeline-content">
                             <h6 class="text-dark text-sm font-weight-bold mb-0 text-start">¿Presenta Insuficiencia Respiratoria?</h6>
                             <div class="d-flex align-items-center mt-2">
-                                ${setIconAndTextTimeLine(response.decision_rules.respiratory_failure)}
+                                ${setIconAndTextTimeLine(
+                                    response.decision_rules.respiratory_failure
+                                )}
                             </div>
                         </div>
                     </div>
@@ -206,7 +216,9 @@ function displayTimeLineHTML(response) {
                         <div class="timeline-content">
                             <h6 class="text-dark text-sm font-weight-bold mb-0 text-start">¿Presenta Síndrome de dificultad Respiratoria?</h6>
                             <div class="d-flex align-items-center mt-2">
-                                ${setIconAndTextTimeLine(response.decision_rules.ards)}
+                                ${setIconAndTextTimeLine(
+                                    response.decision_rules.ards
+                                )}
                             </div>
                         </div>
                     </div>
@@ -217,7 +229,9 @@ function displayTimeLineHTML(response) {
                         <div class="timeline-content">
                             <h6 class="text-dark text-sm font-weight-bold mb-0 text-start">¿Presenta shock séptico?</h6>
                             <div class="d-flex align-items-center mt-2">
-                                ${setIconAndTextTimeLine(response.decision_rules.sepsis_shock)}
+                                ${setIconAndTextTimeLine(
+                                    response.decision_rules.sepsis_shock
+                                )}
                             </div>
                         </div>
                     </div>
@@ -228,47 +242,88 @@ function displayTimeLineHTML(response) {
 }
 
 function setSeverityTimeLine(caseSeverity) {
-    if(caseSeverity.toLowerCase() === 'moderado'){
-        return `<span class="badge moderate-bg badge-md">${caseSeverity}</span>`
+    if (caseSeverity.toLowerCase() === 'moderado') {
+        return `<span class="badge moderate-bg badge-md">${caseSeverity}</span>`;
     }
 
-    if(caseSeverity.toLowerCase() === 'grave'){
-        return `<span class="badge serius-bg badge-md">${caseSeverity}</span>`
+    if (caseSeverity.toLowerCase() === 'grave') {
+        return `<span class="badge serius-bg badge-md">${caseSeverity}</span>`;
     }
 
-    return `<span class="badge critical-bg badge-md">${caseSeverity}</span>`
+    return `<span class="badge critical-bg badge-md">${caseSeverity}</span>`;
 }
 
 function setIconTimeLine(condition) {
-    if(condition) {
+    if (condition) {
         return `
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" focusable="false" class="td-icon text-danger" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
             </svg>
-        `
+        `;
     }
     return `
         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" focusable="false" class="td-icon text-success" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
             <path fill="none" d="M0 0h24v24H0z"></path>
             <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path>
         </svg>
-    `
-} 
+    `;
+}
 
 function setIconAndTextTimeLine(condition) {
-    if(condition) {
+    if (condition) {
         return `
             ${setIconTimeLine(condition)}
             <p class="text-sm text-secondary fw-bolder mb-0">SI</p>
-        `
+        `;
     }
     return `
         ${setIconTimeLine(condition)}
         <p class="text-sm text-secondary fw-bolder mb-0">NO</p>
-    `
-} 
+    `;
+}
 
-function setGeneralTooltip(tooltipId){
+function setGeneralTooltip(tooltipId) {
     return new bootstrap.Tooltip(tooltipId, {});
 }
+
+initializeClinicalTooltips();
+
+pao2Input.addEventListener('keyup', () => {
+    calculatePfRatio();
+});
+
+fio2Input.addEventListener('keyup', () => {
+    calculatePfRatio();
+});
+
+patientForm.addEventListener('submit', (event) => {
+    if (patientForm.checkValidity()) {
+        var patientFormData = formDataToJson(event);
+        sendPatient(patientFormData);
+    } else {
+        emptyFormAlert();
+    }
+    event.preventDefault();
+});
+
+var unsaved = false;
+
+$(':input').change(function () {
+    unsaved = true;
+});
+
+function unloadPage() {
+    if (unsaved) {
+        unsavedChangesAlert();
+    }
+}
+window.onbeforeunload = unloadPage;
+
+// https: $(':input').change(() => {
+//     $(window)
+//         .unbind('unload')
+//         .bind('unload', () => {
+//             unsavedChangesAlert();
+//         });
+// });
