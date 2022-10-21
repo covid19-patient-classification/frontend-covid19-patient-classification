b2ee0bcdf910dfc5fa0824ddb8c7d16d701a7c45
carloscastillo10 <carloscastillo090916@gmail.com>
2022-10-21 12:16:12 -0500

    :zap: Refactor code

--- app/home/static/assets/js/preloader.js
+++ app/home/static/assets/js/preloader.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/* eslint-disable no-undef */
+document.addEventListener("DOMContentLoaded", function() {
     var preloader = document.querySelector('.preloader');
     if(preloader) {
         const animations = ['oneByOne', 'delayed', 'sync', 'scenario'];
 
-        new Vivus('loader-logo', {duration: 80, type: 'oneByOne'}, function () {});
+        new Vivus('loader-logo', {duration: 80, type: animations[1]}, function () {});
 
         setTimeout(function() {
             preloader.classList.add('show');
